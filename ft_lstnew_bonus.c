e95cddc3c88e3ab2e66f97ea49440462a84f6790
ayoub elmouafik <145219022+mfk-ayoub@users.noreply.github.com>
2023-11-27 17:18:57 +0100

    Update ft_lstnew_bonus.c

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -25,3 +25,26 @@ t_list	*ft_lstnew(void *content)
 	new->next = NULL;
 	return (new);
 }
+/*
+int main(void)
+{
+	t_list *test;
+	test = NULL;
+
+	t_list *test1 = ft_lstnew(ft_strdup("ayoub"));
+	t_list *test2 = ft_lstnew(ft_strdup("obito"));
+	t_list *test3 = ft_lstnew(ft_strdup("elmfk"));
+
+	test = test1;
+	test1->next = test2;
+	test2->next = test3;
+	test3->next = NULL;
+	t_list *tmp;
+	tmp = test;
+	while (tmp)
+	{
+		printf ("%s\t",(char *)tmp->content);
+		tmp = tmp->next;
+	}
+	
+}*/
