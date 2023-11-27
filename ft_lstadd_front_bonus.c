e676f9cf6940112ce3034506f9db0cfdb6d17621
ayoub elmouafik <145219022+mfk-ayoub@users.noreply.github.com>
2023-11-27 17:30:32 +0100

    Update ft_lstadd_front_bonus.c

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -17,3 +17,31 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	new->next = *lst;
 	*lst = new;
 }
+/*
+#include <stdio.h>
+int main(void)
+{
+    t_list *list = ft_lstnew("World");
+    ft_lstadd_front(&list, ft_lstnew("Hello"));
+
+    int *x = malloc(sizeof(int));
+    *x = 42;
+    ft_lstadd_front(&list, ft_lstnew(x));
+
+    t_list *current = list;
+    while (current != NULL)
+    {
+        if (current->content != NULL)
+        {
+            if (current->content == x)
+                printf("%d\n", *((int *)current->content));
+            else
+                printf("%s\n", (char *)current->content);
+        }
+        current = current->next;
+    }
+	
+    printf("Size of the list: %d\n", ft_lstsize(list));
+    return 0;
+}
+*/
